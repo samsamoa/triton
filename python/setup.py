50009d40ff7636e29f41a16d4e77ad7ddb40a884
Sam McCandlish <sam@anthropic.com>
2021-12-21 23:54:05 -0800

    bump version so it installs

--- python/setup.py
+++ python/setup.py
@@ -121,7 +121,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="triton",
-    version="2.0.1",
+    version="2.0.2",
     author="Philippe Tillet",
     author_email="phil@openai.com",
     description="A language and compiler for custom Deep Learning operations",
